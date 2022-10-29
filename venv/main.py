3c234920c5c21af1a5b87c51e8b80c0fbe76a786
lozin <lozindenis6@gmail.com>
2022-10-29 11:15:27 +0300

    Create work "№5"

--- venv/main.py
+++ venv/main.py
@@ -1,9 +1,11 @@
-def cube(n):
-    a = n*n*n
-    return a
-
+import math
+def cube(b):
+    return math.pow(b,3)
+print(int(cube(5)))
 
 try:
-    cube(5)
+   cube(5)
 except Exception as ex:
     print(f'Eror information: {ex}')
+
+
